47ebb8b6b470beea43d965c4f65a700b8c983c97
chris-marie <cmhamilton2.718@gmail.com>
2015-03-02 15:49:37 -0800

    set up angular

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,13 @@
 var router = require('express').Router();
-var movies = require('../data/movies');
-
-
+var movies = require('../data/movies').movies;
 
 router.get('/', function (req, res) {
-    res.json({greeting: 'hello world!'});
+    if(req.query.page && req.query.page <= 20 && req.query.page >= 1) {
+        var startindex = (+req.query.page -1) * 5;
+        res.status(200).json(movies.slice(startindex, startindex + 5));
+    } else {
+        res.status(200).json(movies.slice(0,5));
+    }
 });
 
 
