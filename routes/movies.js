74781dfddacf1b62324100af3d3d7a0c7d683e0d
chris-marie <cmhamilton2.718@gmail.com>
2015-03-02 14:19:02 -0800

    simplified response obj -trying to debug server crash

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,13 @@
 var router = require('express').Router;
 var movies = require('../data/movies');
 
-console.log(movies);
+
 
 router.get('/', function (req, res) {
-    res.json();
+    res.json({'hi':'hello world'});
 });
+
+
 
 
 
